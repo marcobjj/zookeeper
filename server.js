ffcd8affe063b9c3be02e3ee0bbcca4da9727b99
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-09-28 12:31:03 -0700

    adds dynamic port

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 const { animals } = require('./data/animals');
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
 
