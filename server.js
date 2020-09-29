17cc06152a7e3c9e056d5a629497e34b2bc393fb
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-09-28 20:07:52 -0700

    filter by id and 404

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+      } else {
+        res.send(404);
+      }
+  });
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
